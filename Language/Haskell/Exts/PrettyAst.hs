da6056bc18f499844fb72e8046b35f63878a5f07
Pnom <pnom.pen@gmail.com>
2013-04-26 02:19:24 +0300

    Add several functions for testing

--- Language/Haskell/Exts/PrettyAst.hs
+++ Language/Haskell/Exts/PrettyAst.hs
@@ -26,7 +26,7 @@ import Data.Traversable
 data DocState = DocState {
   pos :: !SrcLoc,
   nestSize :: !Int,
-  traceArr :: ![String]
+  prettifyingTrace :: ![String]
   } deriving Show
 
 defDocState fl = DocState (SrcLoc fl  1  1) 0 []
@@ -2188,4 +2188,62 @@ sInstHead ih = case ih of
   IHead _ qn ts      -> (qn, ts)
   IHInfix _ ta qn tb -> (qn, [ta,tb])
   IHParen _ ih       -> sInstHead ih
+
+-- --------------------------------------------------------------------------------------------------------
+
+setLayoutToDefPRMode l = let m = PR.defaultMode in
+  PR.PPHsMode
+    (classIndent m)
+    (doIndent m)
+    (caseIndent   m)
+    (letIndent    m)
+    (whereIndent  m)
+    (onsideIndent m)
+    (spacing      m)
+    l
+    (linePragmas m)
+
+setLayoutToDefMode l = PrettyMode (setLayoutToDefPRMode l) PR.style
+
+simpleSpanInfo :: SrcSpanInfo -> ((Int, Int, Int, Int), [(Int, Int, Int, Int)])
+simpleSpanInfo s = (simpleSpan $ srcInfoSpan s, map simpleSpan (srcInfoPoints s))
+  where
+    simpleSpan s = (srcSpanStartLine s, srcSpanStartColumn s, srcSpanEndLine s, srcSpanEndColumn s)
+
+testDoc l f = do
+  putStrLn ""
+  putStrLn $ "File: " ++ f ++ "; Layout: " ++
+    case l of { PPOffsideRule -> "PPOffsideRule"; PPSemiColon -> "PPSemiColon"; PPInLine -> "PPInLine"; PPNoLayout -> "PPNoLayout" }
+
+  ParseOk parsingRes <- parseFile f
+  putStrLn "raw parsing result"
+  putStrLn $ show parsingRes
+  putStrLn ""
+  putStrLn "short parsing result"
+  putStrLn . show $ fmap simpleSpanInfo parsingRes
+  putStrLn ""
+
+  let (prettyRes, prettyState) = runState (runReaderT (astPretty parsingRes) (setLayoutToDefMode l)) (defDocState f)
+  putStrLn "short result of ast prettifying"
+  putStrLn . show $ fmap simpleSpanInfo prettyRes
+  putStrLn ""
+  putStrLn "ast prettifying trace:"
+  putStrLn . show $ prettifyingTrace prettyState
+  putStrLn "----------------------------------------"
+  putStrLn "prettyPrintWithMode parsingRes:"
+  putStrLn $ prettyPrintWithMode (setLayoutToDefPRMode l) parsingRes
+  putStrLn "----------------------------------------"
+  putStrLn "exactPrint parsingRes:"
+  putStrLn $ exactPrint parsingRes []
+  putStrLn "----------------------------------------"
+  putStrLn "exactPrint prettyRes:"
+  putStrLn $ exactPrint prettyRes []
+  putStrLn "----------------------------------------"
+  putStrLn ""
+
+testAll f = do
+  testDoc PPOffsideRule f
+  testDoc PPSemiColon   f
+  testDoc PPInLine      f
+  testDoc PPNoLayout    f
 
